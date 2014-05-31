8066a3e1c73500d00db88fb8a4e9af7c04eb6f69
malick youla <myoula@videotron.ca>
2014-05-31 21:25:45 +0200

    code cleaning

--- MYWebservice/UIImageView+Network.m
+++ MYWebservice/UIImageView+Network.m
@@ -15,6 +15,7 @@ static char URL_KEY;
 
 @dynamic imageURL;
 
+// Use this NSCache singleton
 - (NSCache *)imageCache {
     static NSCache *imageCache = nil;
     static dispatch_once_t onceToken;
@@ -29,47 +30,43 @@ static char URL_KEY;
 
 - (void) loadImageFromURL:(NSURL*)url placeholderImage:(UIImage*)placeholder
                cachingKey:(NSString*)key {
-
+    
     self.imageURL = url;
 	self.image = placeholder;
 	
+    // Already cached
 	NSData *cachedData = [[self imageCache] objectForKey:key];
-	if (cachedData) {   
- 	   self.imageURL   = nil;
-        [self loadFromImage:[UIImage imageWithData:cachedData]];
-	   return;
+	if (cachedData) {
+        self.imageURL   = nil;
+        [self loadFromData:cachedData];
+        return;
 	}
-
+    
+    // image not in cached
     dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0ul);
 	dispatch_async(queue, ^{
-        UIImage *imageFromData = nil;
-        
 		NSData *data = [NSData dataWithContentsOfURL:url];
         if (data) {
-            imageFromData = [UIImage imageWithData:data];
-            if (imageFromData) {
-                    if ([self.imageURL.absoluteString isEqualToString:url.absoluteString]) {
-                        [[self imageCache] setObject:data forKey:key];
-                        [self loadFromImage:imageFromData];
-                    } else {
-                        // assert(0);
-                    }
+            if ([self.imageURL.absoluteString isEqualToString:url.absoluteString]) {
+                [[self imageCache] setObject:data forKey:key]; // cache
+                [self loadFromData:data];
             }
         }
 		self.imageURL = nil;
 	});
 }
 
-- (void)loadFromImage:(UIImage *)image {
+// set the image and dimensions
+- (void)loadFromData:(NSData *)data {
     dispatch_async(dispatch_get_main_queue(), ^{
-        self.image = image;
+        self.image = [UIImage imageWithData:data];
         self.frame = CGRectMake(self.frame.origin.x, self.frame.origin.y, self.image.size.width, self.image.size.height);
     });
 }
+
 - (void) setImageURL:(NSURL *)newImageURL {
 	objc_setAssociatedObject(self, &URL_KEY, newImageURL, OBJC_ASSOCIATION_COPY);
 }
-
 - (NSURL*) imageURL {
 	return objc_getAssociatedObject(self, &URL_KEY);
 }
